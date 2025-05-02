03cbbb9bdbcc7c2775f2e4a22f41f3b75256adc0
seojiho <happyluck0426@gmail.com>
2025-05-02 17:44:21 +0900

    fix:스타일 속성 이름 오류 해결

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -191,7 +191,7 @@ const Header = () => {
             <ShoppingCart href="/cart">
                 <ShoppingCartIcon>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 stroke-gray-700 dark:stroke-white" fill="none" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path>
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path>
                     </svg>
                     <ShoppingCartNumber>0</ShoppingCartNumber>
                 </ShoppingCartIcon>
